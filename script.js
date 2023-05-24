6844d56c74272618346180aaf2420f6f4e4e6a20
Victor Torres <vdst2011@hotmail.com>
2023-05-24 18:00:52 -0300

    Inicio cores

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 
 const btok = document.querySelector('#btok')
 const imagemPokemon = document.querySelector('#imagemPokemon')
+const tipos = document.querySelector('#tipos')
 // const pokemon = document.querySelector('#pokemon').value
-const pokemon = ''
+// const pokemon = ''
+
 
 function transformaJson (response){
     return response.json()
 }
 
-function printDados (dados){
- 
-  return dados.sprites.other.dream_world.front_default
+function mostraImagem(dados){
+  imagemPokemon.src = dados.sprites.other.dream_world.front_default
+  return dados
 }
 
-function mostraImagem(imagem){
-  
-  imagemPokemon.src = imagem
+function mostraTipos(dados){
+  console.log(dados.types)
+
+  let listaTipos = []
+  let htmlTipos = ''
+  for (item of dados.types){
+    listaTipos.push(item.type.name)
+    htmlTipos += `<div class="tipo ${item.type.name}"> ${item.type.name} </div>`
+  }
+
+  tipos.innerHTML = htmlTipos
+
+  console.log(listaTipos)
+
 }
 
 function printErro (dados){
@@ -23,15 +36,14 @@ function printErro (dados){
 }
 
 
-btok.onclick = () => 
+btok.onclick = () => {
 
-  pokemon = document.querySelector('#pokemon').value
-
+  const pokemon = document.querySelector('#pokemon').value
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then(transformaJson)
-    .then(printDados)
     .then(mostraImagem)
+    .then(mostraTipos)
     .catch(printErro)
-
+}
 
