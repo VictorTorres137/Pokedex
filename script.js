29b6c54cdf344cba1d2414477222f46690fe12ac
Victor Torres <vdst2011@hotmail.com>
2023-05-25 13:23:40 -0300

    Iniciando gerador de Fraquezas

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ const tipos = document.querySelector('#tipos')
 // const pokemon = ''
 
 
+let listaF = []
+
+
+function geraFraquesas (dados){
+  
+  listaF = []
+
+  for (item of dados.types){
+     
+    fetch(item.type.url)
+    .then(transformaJson)
+    .then(lista)
+    .then(printLista)
+    .catch()
+  }
+
+  return listaFraq
+}
+
 function transformaJson (response){
     return response.json()
 }
@@ -16,7 +35,7 @@ function mostraImagem(dados){
 }
 
 function mostraTipos(dados){
-  console.log(dados.types)
+  
 
   let listaTipos = []
   let htmlTipos = ''
@@ -27,9 +46,48 @@ function mostraTipos(dados){
 
   tipos.innerHTML = htmlTipos
 
-  console.log(listaTipos)
-
+  // console.log(listaTipos)
+  return dados
 }
+
+function lista (dados){
+
+  let listGeral = []
+
+  let list2x = []
+  let list0x = []
+  let list05x = []
+  
+  // console.log(dados.damage_relations)
+  // console.log(dados.damage_relations.double_damage_from)
+  // console.log(dados.damage_relations.half_damage_from)
+  // console.log(dados.damage_relations.no_damage_from)
+
+  for (item of dados.damage_relations.double_damage_from){
+   list2x.push(item.name)
+  }
+  for (item of dados.damage_relations.half_damage_from){
+    list05x.push(item.name)
+  }
+  for (item of dados.damage_relations.no_damage_from){
+    list0x.push(item.name)
+  }
+
+  listGeral.push(list2x)
+  listGeral.push(list0x)
+  listGeral.push(list05x)
+
+  listaF.push(listGeral)
+
+  const listaFraq = listaF
+  
+  return listaFraq
+}
+
+function printLista(item){
+  console.log(item)
+}
+
 
 function printErro (dados){
   console.log('Erro')
@@ -44,6 +102,7 @@ btok.onclick = () => {
     .then(transformaJson)
     .then(mostraImagem)
     .then(mostraTipos)
+    .then(geraFraquesas)
     .catch(printErro)
 }
 
