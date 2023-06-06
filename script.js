0a5fdbbdc13126247797bd5c449782c6f0f03672
Victor Torres <vdst2011@hotmail.com>
2023-06-06 18:27:14 -0300

    logica fraquesas

--- script.js
+++ script.js
@@ -1,96 +1,133 @@
 
+
 const btok = document.querySelector('#btok')
+const btok2 = document.querySelector('#btok2')
 const imagemPokemon = document.querySelector('#imagemPokemon')
 const tipos = document.querySelector('#tipos')
+const d4x = document.querySelector('#d4x')
+const d2x = document.querySelector('#d2x')
+const d1x = document.querySelector('#d1x')
+const d1_2x = document.querySelector('#d1_2x')
+const d1_4x = document.querySelector('#d1_4x')
+const d0x = document.querySelector('#d0x')
 // const pokemon = document.querySelector('#pokemon').value
 // const pokemon = ''
 
-const listaTiposGeral = [
-  { tipo: 'normal', dano: 1 },
-  { tipo: 'fire', dano: 1 },
-  { tipo: 'water', dano: 1 },
-  { tipo: 'grass', dano: 1 },
-  { tipo: 'flying', dano: 1 },
-  { tipo: 'poison', dano: 1 },
-  { tipo: 'electric', dano: 1 },
-  { tipo: 'ground', dano: 1 },
-  { tipo: 'rock', dano: 1 },
-  { tipo: 'psychic', dano: 1 },
-  { tipo: 'ice', dano: 1 },
-  { tipo: 'bug', dano: 1 },
-  { tipo: 'ghost', dano: 1 },
-  { tipo: 'steel', dano: 1 },
-  { tipo: 'dragon', dano: 1 },
-  { tipo: 'dark', dano: 1 },
-  { tipo: 'fairy', dano: 1 } 
-]
 
 let listaF = []
 
-function criaLista(item){
-  let lFraq = []
-  // console.log("cria lista", item.damage_relations)
 
 
-  for (ob of item.damage_relations.double_damage_from){
-    lFraq.push(ob.name)
-  }
-
-  // console.log(lFraq)
-
-  return lFraq
-
-}
-
-function n (p){
-
-  console.log("P", p)
-  
-}
-
-function atuaListFim (p){
-
-  console.log("P", p)
-  b.then(n)
-}
 
 function geraFraquesas (dados){
+  function multtipos (item){
+    let listRelevantes = []
+    
 
-  listaF = []
-  listaRelacoes = []
-  let a = 'a'
-  let b = 'b'
+    for (tipo of item.damage_relations.double_damage_from){
+      listRelevantes.push({tipo:tipo["name"], dano:2})
+    }
+    for (tipo of item.damage_relations.half_damage_from){
+      listRelevantes.push({tipo:tipo["name"], dano:0.5})
+    }
+    for (tipo of item.damage_relations.no_damage_from){
+      listRelevantes.push({tipo:tipo["name"], dano:0})
+    }
 
+    return listRelevantes
+
+  }
+  let rel1
+  let rel2 = false
+  let listaGeral = [
+    {"tipo":"normal", dano: 1},
+    {"tipo":"fire", dano: 1},
+    {"tipo":"water", dano: 1},
+    {"tipo":"grass", dano: 1},
+    {"tipo":"flying", dano: 1},
+    {"tipo":"fighting", dano: 1},
+    {"tipo":"poison", dano: 1},
+    {"tipo":"electric", dano: 1},
+    {"tipo":"ground", dano: 1},
+    {"tipo":"rock", dano: 1},
+    {"tipo":"psychic", dano: 1},
+    {"tipo":"ice", dano: 1},
+    {"tipo":"bug", dano: 1},
+    {"tipo":"ghost", dano: 1},
+    {"tipo":"steel", dano: 1},
+    {"tipo":"dragon", dano: 1},
+    {"tipo":"dark", dano: 1},
+    {"tipo":"fairy", dano: 1}
+  ]
   
-  console.log("dados",dados)
-  console.log("dados2",dados.types[0].type.url)
 
-  a = fetch(dados.types[0].type.url)
-    .then(transformaJson)
-    .then(criaLista)
-    .catch()
+  let tipos = []
 
-  listaF.push(a)
+  const tipo1 = fetch(`${dados.types[0].type["url"]}`).then(transformaJson)
 
-  console.log(dados.types.length)
+  tipos.push(tipo1)
 
-  if (dados.types.length == 2){
+  if (dados.types[1]){
+    const tipo2 = fetch(`${dados.types[1].type["url"]}`).then(transformaJson)
+    tipos.push(tipo2)
+    // console.log(tipo2)
+  }
+  // na lista tipos tem as promisses dos tipos do pokemon
+  // console.log(listaGeral)
 
-    b = fetch(dados.types[1].type.url)
-    .then(transformaJson)
-    .then(criaLista)
-    .catch()
-    listaF.push(b)
+  function printFraq(item){
+    
     
   }
 
+  if (tipos[1]){
+    
+    rel1 = tipos[0].then(multtipos)
+    rel2 = tipos[1].then(multtipos)
 
-  listaRelacoes = a.then(atuaListFim)
- 
+    rel1.then(printFraq)
+    // rel2.then(printFraq)
+  
+  }else{
+    rel1 = tipos[0].then(multtipos)
 
+    rel1.then(printFraq)
+  }
+
+
+  console.log("lista Geral",listaGeral)
+  console.log("lista Geral1",listaGeral[1])
+  // let HTMLFraq = ''
+  for (item of listaGeral){
+    
+    
+    switch(item["dano"]){
+      
+      case 4:
+        d4x.innerHTML += `<div class="tipo ${item["tipo"]}"> ${item["tipo"]} </div>`
+        break;
+      case 2:
+        d2x.innerHTML += `<div class="tipo ${item["tipo"]}"> ${item["tipo"]} </div>`
+        break;
+      case 1:
+        d1x.innerHTML += `<div class="tipo ${item["tipo"]}"> ${item["tipo"]} </div>`
+        break;
+      case 0.5:
+        d1_2x.innerHTML += `<div class="tipo ${item["tipo"]}"> ${item["tipo"]} </div>`
+        break;
+      case 0.25:
+        d1_4x.innerHTML += `<div class="tipo ${item["tipo"]}"> ${item["tipo"]} </div>`
+        break;
+
+    }
+  }
+
+  // tmlTipos += `<div class="tipo ${item.type.name}"> ${item.type.name} </div>`
+  // console.log("rel",rel1)
+  // console.log("rel",rel2)
 }
 
-function transformaJson (response){
+function transformaJson(response){
     return response.json()
 }
 
@@ -144,31 +181,18 @@ function lista (dados){
 
   listaF.push(listGeral)
 
-  // const listaFraq = listaF
-
-  // console.log(listaFraq)
-
-  return listaGeral
+  var listaFraq = listaF
+  
+  return listaFraq
 }
 
 function printLista(item){
-  // console.log(item)
-  for (valor of item){
-      for(val2x of valor[0]){
-        for(valListaGeral of listaTiposGeral){
-          if(val2x == valListaGeral.tipo){
-            valListaGeral.dano*=2
-          }
-        }
-      }
-  }
-  console.log(listaTiposGeral)
-
+  console.log(item)
 }
 
 
 function printErro (dados){
-  // console.log('Erro')
+  console.log("erro",dados)
 }
 
 
@@ -181,7 +205,12 @@ btok.onclick = () => {
     .then(mostraImagem)
     .then(mostraTipos)
     .then(geraFraquesas)
-    .then(printLista)
     .catch(printErro)
 }
+
+btok2.onclick = () => {
+  console.log(listaFraq)
+
+}
+
 
